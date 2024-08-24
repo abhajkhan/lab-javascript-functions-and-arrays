23b780d6e7c203aac1e1a0cbc63b3652843aa1aa
Abhaj Khan <abhajkhan02@gmail.com>
2024-08-24 18:29:51 +0530

    iteration1: maxOfTwoNumbers- done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,11 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1>num2){
+    return num1+ " is greater than "+num2;
+  }else{
+    return num2 +" is greater than " +num1;
+  }
+}
 
 
 
