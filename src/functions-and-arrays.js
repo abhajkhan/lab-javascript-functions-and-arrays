61866e6f7fb16b68acdbe1d07fb46b2b70991223
Abhaj Khan <abhajkhan02@gmail.com>
2024-08-25 21:29:00 +0530

    iteration6: ret does word exists-done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -97,7 +97,9 @@ function uniquifyArray(arr) {
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(arr,element) {
+  return arr.includes(element);
+}
 
 
 
